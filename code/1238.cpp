a729915744e8007c97698a95ae840e7098ff9ba2
jin5866 <hjl5866@naver.com>
2021-08-05 22:14:54 +0900

    1238

---
+++ code/1238.cpp
@@ -0,0 +1,162 @@
+// back.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+// 
+
+#include <iostream>
+#include<bitset>
+#include <queue>
+//#include<priority_queue>
+#include<stack>
+#include<algorithm>
+#include <vector>
+#include <utility>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<memory>
+#include<string>
+#include<algorithm>
+
+template<typename T>
+void debugprint(T t) {
+	std::cout << t;
+}
+
+
+std::pair<int, int> getfarest(std::vector<std::vector<std::pair<int, int>>>& link, int from) {
+
+	std::set<int> visitlist{};
+
+	std::queue<std::pair<int, int>> qu;
+
+	std::pair<int, int> result = { from,0 };
+
+	qu.push({ from,0 });
+
+	while (!qu.empty()) {
+		std::pair<int, int> now = qu.front();
+		qu.pop();
+
+		if (visitlist.find(now.first) != visitlist.end()) {
+			continue;
+		}
+
+		visitlist.insert(now.first);
+
+		if (result.second < now.second) {
+			result = now;
+		}
+
+		for (std::pair<int, int> l : link[now.first]) {
+			qu.push({ l.first,now.second + l.second });
+		}
+	}
+
+	return result;
+}
+
+
+class cmp {
+public:
+	bool operator() (const std::pair<int, int>& a, const std::pair<int, int>& b) {
+		return a.second > b.second;
+	}
+};
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+
+	const int inf = 1000000000;
+	
+
+	int N, M, X;
+	std::cin >> N >> M >> X;
+
+	std::vector<std::vector<int>> link(N + 1, std::vector<int>( N + 1 , inf));
+
+	std::vector<int> fromdis(N + 1, inf);
+	std::vector<int> todis(N + 1, inf);
+
+	for (int i = 0; i < M; i++) {
+		int a, b, c;
+		std::cin >> a >> b >> c;
+
+		link[a][b] = c;
+	}
+	
+
+	std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, cmp> qu;
+
+	qu.push({ X,0 });
+	fromdis[X] = 0;
+	std::set<int> isvisit;
+	while (!qu.empty()) {
+		std::pair<int, int> now = qu.top();
+		qu.pop();
+
+		if (isvisit.find(now.first) != isvisit.end()) {
+			continue;
+		}
+
+		isvisit.insert(now.first);
+
+		for (int i = 1; i <= N; i++) {
+			if (link[now.first][i] != inf) {
+				if (fromdis[i] > fromdis[now.first] + link[now.first][i]) {
+					fromdis[i] = fromdis[now.first] + link[now.first][i];
+				}
+
+				qu.push({ i,(fromdis[i]) });
+			}
+		}
+	}
+
+	qu.push({ X,0 });
+	isvisit.clear();
+	todis[X] = 0;
+	while (!qu.empty()) {
+		std::pair<int, int> now = qu.top();
+		qu.pop();
+
+		if (isvisit.find(now.first) != isvisit.end()) {
+			continue;
+		}
+
+		isvisit.insert(now.first);
+
+		for (int i = 1; i <= N; i++) {
+			if (link[i][now.first] != inf) {
+				if (todis[i] > todis[now.first] + link[i][now.first]) {
+					todis[i] = todis[now.first] + link[i][now.first];
+				}
+
+				qu.push({ i,(todis[i]) });
+			}
+		}
+	}
+	
+	
+
+	
+	int re = 0;
+	for (int from = 1; from <= N; from++) {
+		re = std::max(fromdis[from] + todis[from], re);
+	}
+
+	std::cout << re << "\n";
+}
+
+
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
