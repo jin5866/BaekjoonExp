8f7eaee71bd1ce05c20d8b018001cebc485d6414
jin5866 <hjl5866@naver.com>
2021-08-16 03:23:15 +0900

    10830

---
+++ code/10830.cpp
@@ -0,0 +1,106 @@
+// back.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+// 
+
+#include <iostream>
+#include<bitset>
+#include <queue>
+//#include<priority_queue>
+#include<stack>
+#include<algorithm>
+#include <vector>
+#include <utility>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<memory>
+#include<string>
+#include<algorithm>
+
+template<typename T>
+void debugprint(T t) {
+	std::cout << t;
+}
+
+const int mod = 1000;
+
+std::vector<std::vector<int>> mul(std::vector<std::vector<int>>& a, std::vector<std::vector<int>>& b, int n) {
+	std::vector<std::vector<int>> re(n, std::vector<int>(n, 0));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			int acc = 0;
+			for (int k = 0; k < n; k++) {
+				acc += a[i][k] * b[k][j];
+			}
+
+			acc %= mod;
+
+			re[i][j] = acc;
+		}
+	}
+
+	return re;
+}
+
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cout.tie(0);
+	std::cin.tie(0);
+	
+
+	int n;
+
+	long long b;
+
+	std::cin >> n >> b;
+
+	std::vector<std::vector<int>> input(n, std::vector<int>(n, 0));
+
+	for (auto& l : input) {
+		for (int& i : l) {
+			std::cin >> i;
+		}
+	}
+
+	long long pow = 1;
+	std::vector<std::vector<int>> accu(n, std::vector<int>(n, 0));
+
+	for (int i = 0; i < n; i++) {
+		accu[i][i] = 1;
+	}
+
+
+	std::vector<std::vector<int>> pownow = input;
+
+	while (pow <= b) {
+		if (pow & b) {
+			accu = mul(accu, pownow, n);
+		}
+
+		pow <<= 1;
+		pownow = mul(pownow, pownow, n);
+	}
+
+	for (auto& l : accu) {
+		for (int& i : l) {
+			std::cout << i << " ";
+		}
+		std::cout << "\n";
+	}
+	//return 0;
+}
+
+
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
